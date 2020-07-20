456bda3a4f2de205a855487fdb75edffd217976c
Megan Spiers <Spiers4.13@gmail.com>
2020-07-20 10:11:06 -0400

    Added All Features Except Sorting

--- CPUSCHED/src/processScheduling.java
+++ CPUSCHED/src/processScheduling.java
@@ -13,13 +13,21 @@ public class processScheduling {
 	
 	
 	
-	public static int displayMenu(PrintWriter outputFile){
+	
+	
+	
+	
+	public static void displayMenu(PrintWriter outputFile){
 		
         Scanner scnrInput = new Scanner(System.in);
+        String orderSelected = "";
+        
+        System.out.println("Initial Note: Results Are Also Logged in the ResultsLog.txt File\n");
+        outputFile.println("RESULTS LOG");
         
         while(true) {
         	
-            System.out.println("Choose One of the Orders Below:\n");
+            System.out.println("\nChoose One of the Orders Below:\n");
             System.out.println("1. FIFO");
             System.out.println("2. SJF W/O Preemption");
             System.out.println("3. Priority W/ Preemption");
@@ -35,29 +43,42 @@ public class processScheduling {
             	
                         case "1":
                         	
-                        	outputFile.println("\nOption 1 Test\n");
+                        	orderSelected = "FIFO";
+                        	displayOutputToConsole(orderSelected, 10000, 400, 4, 29, 3, 2, 1);
+                        	logOutputToFile(outputFile, orderSelected, 10000, 400, 4, 29, 3, 2, 1);
                             break;
                             
                             
                         case "2":
                         	
-                        	System.out.println("\nOption 2 Test\n");
+                        	orderSelected = "SJF W/O Preemption";
+                        	displayOutputToConsole(orderSelected, 10000, 500, 5, 75, 7, 9, 2);
+                        	logOutputToFile(outputFile, orderSelected, 10000, 500, 5, 75, 7, 9, 2);
                             break;
                             
                             
                         case "3":
                         	
-                        	System.out.println("\nOption 3 Test\n");
+                        	orderSelected = "Priority W/ Preemption";
+                        	displayOutputToConsole(orderSelected, 10000, 783, 1, 46, 8, 8, 5);
+                        	logOutputToFile(outputFile, orderSelected, 10000, 783, 1, 46, 8, 8, 5);
                         	break;
                         	
                         	
                         case "4":
                         	
+                        	orderSelected = "Sixty-Six";
+                        	
+                        	System.out.println("\nOrder Selected: " + orderSelected);
                         	System.out.println("\nGood Soldiers Follow Orders...");
-                        	System.out.println("Program Terminated");
+                        	System.out.println("Program Terminated, Results Logged");
+                        	
+                        	outputFile.println("\nOrder Selected: " + orderSelected);
+                        	outputFile.println("\nGood Soldiers Follow Orders...");
+                        	outputFile.println("Program Terminated");
                         	
                         	scnrInput.close();
-                        	return 0;
+                        	return;
                         	
                }
                         
@@ -74,11 +95,55 @@ public class processScheduling {
 	}
 
 	
+	
+	public static void logOutputToFile(PrintWriter outputFile, String orderSelected, 
+			int numProcesses, double totalTime, double throughput, double CPUutilization, 
+			double averageWaitTime, double averageTurnaroundTime, double averageResponseTime) {
+		
+		outputFile.println("\nOrder Selected: " + orderSelected);
+		outputFile.println("Statistics for the Run\n");
+		
+		outputFile.println("Number of Processes: " + numProcesses);
+		outputFile.println("Total Elapsed Time (For the Scheduler): " + totalTime);
+		outputFile.println("Throughput: " + throughput);
+		outputFile.println("CPU Utilization: " + CPUutilization + "%");
+		outputFile.println("Average Waiting Time: " + averageWaitTime);
+		outputFile.println("Average Turnaround Time: " + averageTurnaroundTime);
+		outputFile.println("Average Response Time: " + averageResponseTime + "\n");
+		
+		return;
+	}
+	
+	public static void displayOutputToConsole(String orderSelected, 
+			int numProcesses, double totalTime, double throughput, double CPUutilization, 
+			double averageWaitTime, double averageTurnaroundTime, double averageResponseTime) {
+		
+		System.out.println("\nOrder Selected: " + orderSelected);
+		System.out.println("Statistics for the Run\n");
+		
+		System.out.println("Number of Processes: " + numProcesses);
+		System.out.println("Total Elapsed Time (For the Scheduler): " + totalTime);
+		System.out.println("Throughput: " + throughput);
+		System.out.println("CPU Utilization: " + CPUutilization + "%");
+		System.out.println("Average Waiting Time: " + averageWaitTime);
+		System.out.println("Average Turnaround Time: " + averageTurnaroundTime);
+		System.out.println("Average Response Time: " + averageResponseTime + "\n");
+		
+		return;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) throws IOException {
 		
 		
 	    FileInputStream inputFile = new FileInputStream("RandomInput.txt");
-	    PrintWriter outputFile = new PrintWriter("StatisticsfortheRun.txt");
+	    PrintWriter outputFile = new PrintWriter("ResultsLog.txt");
 
 	      
 	    Scanner scnr = new Scanner(inputFile);
